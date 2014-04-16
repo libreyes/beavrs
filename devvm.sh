051b7906145a6851da697190441b898b52b94e1a
James Laver <james@codegems.org>
2014-04-16 15:28:42 +0100

    Added devvm script to start vagrant with our preferred args

---
+++ devvm.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pushd vagrant
+PHPDEVVM_NAME=beavrs \
+PHPDEVVM_HOST_IP=172.16.0.20 \
+PHPDEVVM_EXPORT_DIR=../htdocs \
+PHPDEVVM_MYSQL=1 \
+PHPDEVVM_CPUS=1 \
+PHPDEVVM_MEMORY=512 \
+PHPDEVVM_HTTP_PORT=9020 \
+PHPDEVVM_MYSQL_PORT=9120 \
+vagrant up
+vagrant ssh
+popd
